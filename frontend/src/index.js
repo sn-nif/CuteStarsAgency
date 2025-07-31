0a39048399ea978650dd87d0f0e7f01f076b3f9e
sn-nif <forzakinder10@gmail.com>
2025-07-31 23:07:06 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'react-phone-input-2/lib/style.css'; // ✅ Add this line
 import "./index.css";
 import App from "./App";
 
