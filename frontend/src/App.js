fe802ec04e2327bc4268c20969d9e0739096b749
sn-nif <forzakinder10@gmail.com>
2025-08-07 21:12:12 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import ApplicationForm from "./pages/ApplicationForm";
+import ThankYou from "./pages/ThankYou";
+import HomePage from "./pages/HomePage";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/apply" element={<ApplicationForm />} />
+        <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
